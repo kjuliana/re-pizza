094902e7f6e139d0b5d32fbc22623af1492fa50a
kjuliana <korjuliana@gmail.com>
2023-02-07 20:55:47 +0300

    Product cards on page layout

--- pages/catalog/index.tsx
+++ pages/catalog/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Layout from "../../components/Layout/Layout";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import styles from './index.module.css';
 
 const Catalog = () => {
     return (
         <Layout>
-            Каталог
-            <ProductCard name={'ляля'} price={4} count={4} image={'dfb'}/>
+            <div className={styles.catalog}>
+                <ProductCard name={'Бананы'} price={250} count={5} image={'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg'}/>
+                <ProductCard name={'Бананы'} price={250} count={5} image={'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg'}/>
+                <ProductCard name={'Бананы'} price={250} count={5} image={'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg'}/>
+                <ProductCard name={'Бананы'} price={250} count={5} image={'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg'}/>
+                <ProductCard name={'Бананы'} price={250} count={5} image={'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg'}/>
+                <ProductCard name={'Бананы'} price={250} count={5} image={'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg'}/>
+                <ProductCard name={'Бананы'} price={250} count={5} image={'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg'}/>
+            </div>
         </Layout>
     );
 };

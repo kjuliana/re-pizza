a2940ebb1f520778d48016f3b4ce217ee9c2d0ad
kjuliana <korjuliana@gmail.com>
2023-02-09 16:50:17 +0300

    optimal css

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -12,11 +12,13 @@ interface ProductCardProps {
 const ProductCard = ({name, price, count, image}: ProductCardProps) => {
     return (
         <div className={styles.root}>
-            <img className={styles.image} src={image} alt={name}/>
-            <div className={styles.about}>
-                <span className={styles.bold}>{price} ₽</span>
-                <span className={styles.mute}>/ {count} шт</span>
-                <p>{name}</p>
+            <div className={styles.content}>
+                <img className={styles.image} src={image} alt={name}/>
+                <div className={styles.about}>
+                    <span className={styles.bold}>{price} ₽</span>
+                    <span className={styles.mute}>/ {count} шт</span>
+                    <p>{name}</p>
+                </div>
             </div>
             <MainButton>
                 Купить
--- pages/catalog/index.tsx
+++ pages/catalog/index.tsx
@@ -4,16 +4,86 @@ import ProductCard from "../../components/ProductCard/ProductCard";
 import styles from './index.module.css';
 
 const Catalog = () => {
+    const catalog = [
+        {
+            id: 1,
+            name: 'Бананы с запиской-пожеланием',
+            price: 1250,
+            count: 5,
+            image: 'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg',
+        },
+        {
+            id: 2,
+            name: 'Турецкие бананы',
+            price: 400,
+            count: 5,
+            image: 'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg',
+        },
+        {
+            id: 3,
+            name: 'Атлантические бананы',
+            price: 3000,
+            count: 5,
+            image: 'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg',
+        },
+        {
+            id: 4,
+            name: 'Грузинские бананы',
+            price: 2500,
+            count: 5,
+            image: 'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg',
+        },
+        {
+            id: 5,
+            name: 'Смоленские бананы',
+            price: 150,
+            count: 5,
+            image: 'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg',
+        },
+        {
+            id: 6,
+            name: 'Астраханские бананы',
+            price: 250,
+            count: 6,
+            image: 'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg',
+        },
+        {
+            id: 7,
+            name: 'Подмосковные бананы',
+            price: 600,
+            count: 4,
+            image: 'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg',
+        },
+        {
+            id: 8,
+            name: 'Искусственные бананы',
+            price: 250,
+            count: 5,
+            image: 'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg',
+        },
+        {
+            id: 9,
+            name: 'Бананы с запахом летних дынь',
+            price: 350,
+            count: 1,
+            image: 'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg',
+        },
+        {
+            id: 10,
+            name: 'Бананы обыкновенные',
+            price: 200,
+            count: 5,
+            image: 'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg',
+        },
+
+    ];
+
     return (
         <Layout>
             <div className={styles.catalog}>
-                <ProductCard name={'Бананы'} price={250} count={5} image={'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg'}/>
-                <ProductCard name={'Бананы'} price={250} count={5} image={'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg'}/>
-                <ProductCard name={'Бананы'} price={250} count={5} image={'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg'}/>
-                <ProductCard name={'Бананы'} price={250} count={5} image={'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg'}/>
-                <ProductCard name={'Бананы'} price={250} count={5} image={'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg'}/>
-                <ProductCard name={'Бананы'} price={250} count={5} image={'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg'}/>
-                <ProductCard name={'Бананы'} price={250} count={5} image={'https://megatitan.ru/upload/iblock/865/wommylb9wv2demew28g7r91gx0qxmz9d.jpg'}/>
+                {catalog.map((item)=>
+                    <ProductCard key={item.id} name={item.name} price={item.price} count={item.count} image={item.image}/>
+            )}
             </div>
         </Layout>
     );
